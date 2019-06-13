25bc5eb418abeb42e9806a184212f53cf47d2b14
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-13 15:17:18 +0100

    Changed from folders to a single file for each chapter.

---
+++ Stylish-FSharp/Chapter2.fs
@@ -0,0 +1,43 @@
+﻿module Chapter2
+
+module Exercise1 =
+
+    open System
+
+    module MilesYards = 
+
+        type MilesYards = 
+            private MilesYards of wholeMiles : int * yards : int
+
+        let fromMilesPointYards (milesPointYards : float) : MilesYards =
+            let wholeMiles = milesPointYards |> floor |> int
+            let fraction = milesPointYards - float(wholeMiles)
+            if fraction > 0.1759 then
+                raise <| ArgumentOutOfRangeException("milesPointYards", "Fractional part must be <= 0.1759")
+            elif fraction < 0. then
+                raise <| ArgumentOutOfRangeException("milesPointYards", "Fractional part must be >= 0")
+            let yards = fraction * 10_000. |> round |> int
+            MilesYards(wholeMiles, yards)
+
+        let toDecimalMiles (MilesYards(wholeMiles, yards)) : float =
+            (float wholeMiles) + ((float yards) / 1760.)
+
+module Exercise2 =
+
+    open System
+
+    module MilesChains =
+
+        type MilesChains =
+            private MilesChains of wholeMiles : int * chains : int
+
+        let fromMilesChains (miles, chains) =
+            if miles < 0 then
+                raise <| ArgumentOutOfRangeException("miles", "Must be positive.")
+            elif chains < 0 || chains > 80 then
+                raise <| ArgumentOutOfRangeException("chains", "Must be between 0 and 80.")
+
+            MilesChains (miles, chains)
+
+        let toDecimalMiles (MilesChains(miles, chains)) =
+            (float miles) + ((float chains) / 80.)
--- Stylish-FSharp/Chapter3.fs
+++ Stylish-FSharp/Chapter3.fs
@@ -1,4 +1,6 @@
-﻿module Exercise1
+﻿module Chapter3
+
+module Exercise1 =
 
     type Delivery =
     | AsBilling
@@ -33,3 +35,16 @@
             match billing.delivery with
             | ClickAndCollect id when id = store -> Some billing
             | _ -> None)
+
+module Exercise2 =
+
+    type BillingDetails = {
+        name : string
+        billing :  string
+        delivery : string option }
+
+    let countOfBillings (billings: BillingDetails seq) =
+        billings
+        |> Seq.map (fun x -> x.billing)
+        |> Seq.map Option.ofObj
+        |> Seq.sumBy Option.count
