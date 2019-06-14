49480081904503a555f94dea78581dc4bfb598e6
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-14 09:18:46 +0100

    Chapter 5 solutions.

---
+++ Stylish-FSharp/Chapter5.fs
@@ -0,0 +1,23 @@
+﻿module Chapter5
+
+module Exercise1 =
+
+    let clip max s = s |> Seq.map (min max)
+
+module Exercise2 =
+    
+    open System
+
+    let extremesMut s =
+        let mutable min = Double.MaxValue
+        let mutable max = Double.MinValue
+        for item in s do
+            if item < min then
+                min <- item
+            if item > max then
+                max <- item
+        min, max
+
+    let extremes s =
+        s |> Seq.min,
+        s |> Seq.max
