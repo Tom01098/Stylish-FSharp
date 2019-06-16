1c894b411781a86ce50a6d0c16bbc1e11fc88a1a
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-16 21:32:59 +0100

    Chapter 6 exercise 3.

--- Stylish-FSharp/Chapter6.fs
+++ Stylish-FSharp/Chapter6.fs
@@ -43,3 +43,41 @@ module Exercise2 =
     fruits
     |> List.iter (fun { Name = name; Count = count } ->
         printfn "There are %i %s" count name)
+
+module Exercise3 =
+    
+    open System
+    open System.Text.RegularExpressions
+
+    let zipCodes = [
+        "90210"
+        "94043"
+        "94043-0138"
+        "10013"
+        "90210-3124"
+        "1OO13" ]
+        
+    let (|USZipCode|_|) s =
+        let m = Regex.Match(s, @"^(\d{5})$")
+        if m.Success then
+            USZipCode s |> Some
+        else
+            None
+
+    let (|USZipPlus4Code|_|) s =
+        let m = Regex.Match(s, @"^(\d{5})-(\d{4})$")
+
+        if m.Success then
+            Some (m.Groups.[1].Value, m.Groups.[2].Value)
+        else
+            None
+                
+    zipCodes
+    |> List.iter (fun z ->
+        match z with
+        | USZipCode c ->
+            printfn "A normal zip code: %s" c
+        | USZipPlus4Code(code, suffix) ->
+            printfn "A Zip+4 code: prefix %s, suffix %s" code suffix
+        | _ as n ->
+            printfn "Not a zip code: %s" n)
