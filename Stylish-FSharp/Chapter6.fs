27df2139ea8f07cffef61387dd9f77537b3db38a
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-16 21:18:51 +0100

    Chapter 6 exercise 2.

--- Stylish-FSharp/Chapter6.fs
+++ Stylish-FSharp/Chapter6.fs
@@ -19,3 +19,27 @@ module Exercise1 =
             sprintf "Your reading on: %s was %07i" dateStr reading
         | Economy7(Day=day; Night=night) ->
             sprintf "Your readings on: %s: Day: %07i Night: %07i" dateStr day night
+
+module Exercise2 =
+    
+    type FruitBatch = { Name:string; Count:int }
+
+    let fruits =
+        [ 
+            { Name = "Apples"; Count = 3 }
+            { Name = "Oranges"; Count = 4 }
+            { Name = "Bananas"; Count = 2 } 
+        ]
+        
+    // There are 3 Apples
+    // There are 4 Oranges
+    // There are 2 Bananas
+    for { Name = name; Count = count } in fruits do
+        printfn "There are %i %s" count name
+
+    // There are 3 Apples
+    // There are 4 Oranges
+    // There are 2 Bananas
+    fruits
+    |> List.iter (fun { Name = name; Count = count } ->
+        printfn "There are %i %s" count name)
