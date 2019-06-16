d30d42b99a12714bc7efd97ef0e18088bae0ef11
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-16 21:02:54 +0100

    Chapter 6 exercise 1.

---
+++ Stylish-FSharp/Chapter6.fs
@@ -0,0 +1,21 @@
+﻿module Chapter6
+
+module Exercise1 =
+
+    open System
+    
+    type MeterValue =
+        | Standard of int
+        | Economy7 of Day:int * Night:int
+
+    type MeterReading =
+        { ReadingDate : DateTime
+          MeterValue : MeterValue}
+
+    let formatReading { ReadingDate = date; MeterValue = value } =
+        let dateStr = date.ToShortDateString()
+        match value with
+        | Standard reading ->
+            sprintf "Your reading on: %s was %07i" dateStr reading
+        | Economy7(Day=day; Night=night) ->
+            sprintf "Your readings on: %s: Day: %07i Night: %07i" dateStr day night
