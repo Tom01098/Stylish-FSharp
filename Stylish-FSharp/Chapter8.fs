553c5780f15d19f92d440bf5414d0e1b5f247b3b
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-18 20:19:30 +0100

    Chapter 8 exercises 1, 2, and 3.

---
+++ Stylish-FSharp/Chapter8.fs
@@ -0,0 +1,35 @@
+﻿module Chapter8
+
+module Exercise1 =
+
+    type Greyscale(r : byte, g : byte, b : byte) =
+    
+        member __.Level =
+            (int r + int g + int b) / 3 |> byte
+
+module Exercise2 =
+    
+    open System.Drawing
+
+    type Greyscale(r : byte, g : byte, b : byte) =
+
+        new(colour : Color) =
+            Greyscale(colour.R, colour.B, colour.G)
+    
+        member __.Level =
+            (int r + int g + int b) / 3 |> byte
+
+module Exercise3 =
+    
+    open System.Drawing
+
+    type Greyscale(r : byte, g : byte, b : byte) =
+
+        new(colour : Color) =
+            Greyscale(colour.R, colour.B, colour.G)
+    
+        member __.Level =
+            (int r + int g + int b) / 3 |> byte
+
+        override this.ToString() =
+            sprintf "Grayscale(%i)" this.Level
