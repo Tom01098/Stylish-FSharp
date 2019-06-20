c8a77e6882f6b208f684a0b91b2ec7658e2c34d4
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-20 19:58:08 +0100

    Chapter 9 exercises 1 and 2.

---
+++ Stylish-FSharp/Chapter9.fs
@@ -0,0 +1,21 @@
+﻿module Chapter9
+
+module Exercise1 =
+    
+    let applyAndPrint f a b =
+        let r = f a b
+        printfn "%i" r
+
+    let multiply a b = a * b
+
+module Exercise2 =
+    
+    let rangeCounter min max =
+        let mutable current = min
+        fun () ->
+            let before = current
+            if current = max then
+                current <- min
+            else
+                current <- current + 1
+            before
