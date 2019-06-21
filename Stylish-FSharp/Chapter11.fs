f3ecff57de1fe41b50b12db936b046cc89407ec5
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-21 18:52:34 +0100

    Chapter 11 exercise 1.

---
+++ Stylish-FSharp/Chapter11.fs
@@ -0,0 +1,22 @@
+﻿module Chapter11
+
+module Exercise1 =
+    
+    type Outcome<'TSuccess, 'TFailure> =
+           | Success of 'TSuccess
+           | Failure of 'TFailure
+
+    let adapt func input =
+        match input with
+        | Success x -> func x
+        | Failure f -> Failure f
+
+    let passThrough func input =
+        match input with
+        | Success x -> func x |> Success
+        | Failure f -> Failure f
+
+    let passThroughRejects func input =
+        match input with
+        | Success x -> Success x
+        | Failure f -> func f |> Failure
