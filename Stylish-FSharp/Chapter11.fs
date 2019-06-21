90533ce406496cec2b8437dace5254f26dad35c0
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-21 20:30:24 +0100

    Chapter 11 exercise 2.

--- Stylish-FSharp/Chapter11.fs
+++ Stylish-FSharp/Chapter11.fs
@@ -20,3 +20,56 @@ module Exercise1 =
         match input with
         | Success x -> Success x
         | Failure f -> func f |> Failure
+
+module Exercise2 =
+
+       open System
+
+       type Message = 
+           { FileName : string
+             Content : float[] }
+
+       type Reading =
+           { TimeStamp : DateTimeOffset
+             Data : float[] }
+
+       let example =
+           [|
+               { FileName = "2019-02-23T02:00:00-05:00"
+                 Content = [|1.0; 2.0; 3.0; 4.0|] }
+               { FileName = "2019-02-23T02:00:10-05:00"
+                 Content = [|5.0; 6.0; 7.0; 8.0|] }
+               { FileName = "error"
+                 Content = [||] }
+               { FileName = "2019-02-23T02:00:20-05:00"
+                 Content = [|1.0; 2.0; 3.0; Double.NaN|] }
+           |]
+
+       let log s = printfn "Logging: %s" s
+
+       type MessageError =
+           | InvalidFileName of fileName:string
+           | DataContainsNaN of fileName:string * index:int
+
+       let getReading message =
+           match DateTimeOffset.TryParse(message.FileName) with
+           | true, dt -> 
+               let reading = { TimeStamp = dt; Data = message.Content }
+               (message.FileName, reading) |> Ok
+           | false, _ -> 
+               message.FileName |> InvalidFileName |> Error
+
+       let validateData(fileName, reading) =
+           let nanIndex = 
+               reading.Data
+               |> Array.tryFindIndex (Double.IsNaN)
+           match nanIndex with
+           | Some i -> 
+               (fileName, i) |> DataContainsNaN |> Error
+           | None -> 
+               Ok reading
+
+       let logError e =
+           match e with
+           | InvalidFileName fn -> sprintf "Invalid file name: '%s'" fn
+           | DataContainsNaN (fn, i) -> sprintf "NaN at %i in %s" i fn
