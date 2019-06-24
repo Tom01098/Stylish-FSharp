8282efd6ea7dac9d38cc1219f5deeb6dd70352f8
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-24 21:05:02 +0100

    Chapter 12 exercise 3.

--- Stylish-FSharp/Chapter12.fs
+++ Stylish-FSharp/Chapter12.fs
@@ -8,3 +8,15 @@ module Exercise1 =
         (oldTransactions : Transaction[])
         (newTransactions : Transaction[]) =
         Array.append oldTransactions newTransactions
+
+module Exercise3 =
+
+    open System
+    open System.Text
+
+    let buildCsv (data : float[,]) =
+        let strings = data |> Array2D.map (sprintf "%f")
+        let sb = StringBuilder()
+        for row = 0 to Array2D.length1 strings - 1 do
+            sb.AppendLine(String.Join(',', strings.[row, *])) |> ignore
+        sb.ToString()
