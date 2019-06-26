c64ccbd1ccdd8cf4b7bd44a478b510349c7f36a3
Tom Humphreys <Tom01098Code@gmail.com>
2019-06-26 19:24:00 +0100

    Chapter 13 exercise.

---
+++ Stylish-FSharp/Chapter13.fs
@@ -0,0 +1,34 @@
+﻿module Chapter13
+
+module private Files =
+    
+    open System.IO
+    open System.Text.RegularExpressions
+
+    let getAllFilesIn dir =
+        Directory.EnumerateFiles
+                      (dir, "*.*", SearchOption.AllDirectories)
+
+    let nameMatches (regex:Regex) (file:string) =
+        regex.IsMatch(Path.GetFileName(file))
+
+module private FileAttributes =
+    
+    open System.IO
+
+    let hasFlag flag file =
+        let info = FileInfo(file)
+        info.Attributes.HasFlag(flag)
+
+module Exercise1 = 
+
+    open System.IO
+    open System.Text.RegularExpressions
+    open Files
+    open FileAttributes
+
+    let find pattern dir =
+        let re = Regex(pattern)
+        getAllFilesIn dir
+        |> Seq.filter (nameMatches re)
+        |> Seq.filter (hasFlag FileAttributes.ReadOnly)
